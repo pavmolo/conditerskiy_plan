e4e44179d91288a7a32f6d0f80ada7eaf240228f
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-08-30 14:15:52 +0300

    Update fast_planning.py

--- fast_planning.py
+++ fast_planning.py
@@ -3,54 +3,72 @@ import numpy as np
 import pandas as pd
 from io import BytesIO
 from pyxlsb import open_workbook as open_xlsb
+
 def distribute_operations(time_mode_var, cycles, plan):
-    merged_plan = plan.merge(cycles, on='operation', how='left')
-    merged_plan['total_time'] = merged_plan['cycle_time'] * merged_plan['quantity']
-    unique_cells = merged_plan['cell'].unique()
-    dfs = []
+  # Объединяем данные из plan и cycles по операциям
+  merged_plan = plan.merge(cycles, on='operation', how='left')
+  
+  # Вычисляем общее время выполнения для каждой операции
+  merged_plan['total_time'] = merged_plan['cycle_time'] * merged_plan['quantity']
 
-    for cell in unique_cells:
-        cell_operations = merged_plan[merged_plan['cell'] == cell].copy()
-        cell_result = []
+  # Получаем уникальные ячейки
+  unique_cells = merged_plan['cell'].unique()
+  cell_operations['operation'] = pd.Categorical(cell_operations['operation'], categories=plan['operation'], ordered=True)
+  cell_operations = cell_operations.sort_values('operation')
 
-        for _, operation_row in cell_operations.iterrows():
-            operation = operation_row['operation']
-            total_time = operation_row['total_time']
-            cycle_time = operation_row['cycle_time']
+  dfs = []
 
-            time_mode_copy = time_mode_var.copy()
-            time_mode_copy['remaining_time'] = time_mode_copy['working_seconds']
+  for cell in unique_cells:
+      cell_operations = merged_plan[merged_plan['cell'] == cell].copy()
+      time_mode_copy = time_mode_var.copy()
+      time_mode_copy['remaining_time'] = time_mode_copy['working_seconds']
+  
+      cell_result = []
+  
+      for _, time_row in time_mode_copy.iterrows():
+          for _, operation_row in cell_operations.iterrows():
+              operation = operation_row['operation']
+              total_time = operation_row['total_time']
+              cycle_time = operation_row['cycle_time']
 
-            for _, time_row in time_mode_copy.iterrows():
-                if total_time <= 0:
-                    break
+              if total_time <= 0:
+                  continue
+  
 
-                operations_count = np.floor(min(total_time / cycle_time, time_row['remaining_time'] / cycle_time))
+              # Вычисляем, сколько операций можно выполнить в текущем часовом интервале
+              operations_count = np.floor(min(total_time / cycle_time, time_row['remaining_time'] / cycle_time))
+              
 
-                if operations_count > 0:
-                    cell_result.append({
-                        'hour_interval': time_row['hour_interval'],
-                        'operation': operation,
-                        'operations_count': operations_count
-                    })
+              if operations_count > 0:
+                  cell_result.append({
+                      'hour_interval': time_row['hour_interval'],
+                      'operation': operation,
+                      'operations_count': operations_count
+                  })
+  
+                  allocated_time = operations_count * cycle_time
+                  total_time -= allocated_time
+                  time_row['remaining_time'] -= allocated_time
+                  operation_row['total_time'] = total_time
+                  # Обновляем количество операций в plan
+                  idx = cell_operations[cell_operations['operation'] == operation].index[0]
+                  cell_operations.at[idx, 'total_time'] = total_time
+                  
+      if cell_result:  # Проверяем, не пуст ли список
+          df = pd.DataFrame(cell_result)
+          # Устанавливаем порядок для столбца operation
+          df['operation'] = pd.Categorical(df['operation'], categories=current_plan['sku'], ordered=True)
+          df['hour_interval'] = pd.Categorical(df['hour_interval'], categories=time_mode['start'], ordered=True)
+          df['cell'] = cell
+          dfs.append(df.sort_values(by=['operation', 'hour_interval']))
 
-                    allocated_time = operations_count * cycle_time
-                    total_time -= allocated_time
-                    time_row['remaining_time'] -= allocated_time
 
-        if cell_result:
-            df = pd.DataFrame(cell_result)
-            df['operation'] = pd.Categorical(df['operation'], categories=current_plan['sku'], ordered=True)
-            df['hour_interval'] = pd.Categorical(df['hour_interval'], categories=time_mode['start'], ordered=True)
-            df['cell'] = cell
-            dfs.append(df.sort_values(by=['operation', 'hour_interval']))
-
-    return dfs
-
+  return dfs
 
 st.markdown('''<a href="http://kaizen-consult.ru/"><img src='https://www.kaizen.com/images/kaizen_logo.png' style="width: 50%; margin-left: 25%; margin-right: 25%; text-align: center;"></a><p>''', unsafe_allow_html=True)
 st.markdown('''<h1>Приложение для разбивки плана по ячейкам и определения потребности в сырье по часам</h1>''', unsafe_allow_html=True)
 col1, col2 = st.columns(2)
+
 with col1:
   st.markdown('''<h3>Файл с мастер данными</h3>''', unsafe_allow_html=True)
   master_data_file = st.file_uploader("Выберите XLSX файл с мастер данными", accept_multiple_files=False)
@@ -64,6 +82,8 @@ if master_data_file:
     time_mode = pd.read_excel(master_data_file, sheet_name='time_mode')
     cream_data = pd.read_excel(master_data_file, sheet_name='cream_data')
     current_plan = pd.read_excel(plan_file, sheet_name='current_date')
+
+
     # Пример использования
     time_mode_data = {
         'hour_interval': time_mode['start'],
@@ -85,8 +105,10 @@ if master_data_file:
     cycles_df = pd.DataFrame(cycles_data)
     plan_df = pd.DataFrame(plan_data)
     #______________________________________________________________________
-    
-    
+
+
+
+
     dataframes = distribute_operations(time_mode_df, cycles_df, plan_df)
     dfdf = []
     for df in dataframes:
